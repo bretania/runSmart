865c7037d92611d727a2c105585956fd68c32178
Tanya <bretania@yandex.ru>
2020-06-03 16:06:57 +0500

    create block catalog

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -5,6 +5,16 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         prevArrow: '<button type="button" class="slick-prev"><img src="../src/static/img/left-arrow.png"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../src/static/img/right-arrow.png"></img></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../src/static/img/right-arrow.png"></img></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                arrows: false,
+                dots: true,
+                dotsClass: 'slick-dots'
+              }
+            }
+          ]
     });
 });
