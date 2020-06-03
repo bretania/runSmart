e4c07db0f4ceef2394bd3f4cd3784aacf6009c70
Tanya <bretania@yandex.ru>
2020-06-03 16:50:26 +0500

    create tabs

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,20 +1,38 @@
 $(document).ready(function(){
-    $('.slider__iner').slick({
-        arrows: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../src/static/img/left-arrow.png"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../src/static/img/right-arrow.png"></img></button>',
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                arrows: false,
-                dots: true,
-                dotsClass: 'slick-dots'
-              }
-            }
-          ]
-    });
+  $('.slider__iner').slick({
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../src/static/img/left-arrow.png"></img></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../src/static/img/right-arrow.png"></img></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          dots: true,
+          dotsClass: 'slick-dots'
+        }
+      }
+      ]
+  });
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__items').removeClass('catalog__items_active').eq($(this).index()).addClass('catalog__items_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click',function(e){
+        e.preventDefault();
+        $('.catalog-item__product').eq(i).toggleClass('catalog-item__product_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    })
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
 });
